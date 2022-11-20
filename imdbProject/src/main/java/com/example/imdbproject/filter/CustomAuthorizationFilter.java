a924bd9c9fd4769510644496914f2545c43be124
amir-feiz <103141846+amir-feiz@users.noreply.github.com>
2022-11-20 21:00:49 +0330

    custome authorization filter

--- imdbProject/src/main/java/com/example/imdbproject/filter/CustomAuthorizationFilter.java
+++ imdbProject/src/main/java/com/example/imdbproject/filter/CustomAuthorizationFilter.java
@@ -1,5 +1,10 @@
 package com.example.imdbproject.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -7,7 +12,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 
+import static java.util.Arrays.stream;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
@@ -31,8 +39,35 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
             if (authorizationHeader != null  && authorizationHeader.startsWith("Bearer ")){
                 //using bearer to confirm a token is valid and doesn't need any further checking for validation
 
+                try {
+                    String token = authorizationHeader.substring("Bearer ".length());
 
-                //getting the token
+
+                    // defining the same algorithm from customAuthentication
+                    Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+
+                    //creating the verifier with same secrete and algorithm
+                    JWTVerifier jwtVerifier = JWT.require(algorithm).build();
+
+
+                    //decoding the token and verifying it
+                    DecodedJWT decodedJWT = jwtVerifier.verify(token);
+
+
+                    // when we find out that username is valid we get the data
+                    String username = decodedJWT.getSubject();
+
+                    //getting roles
+                    String[] roles = decodedJWT.getClaim("roles").asArray(String.class);
+
+                    //passing collection
+                    Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+                    stream(roles).forEach(role -> authorities.add(new SimpleGrantedAuthority(role)));
+
+                }catch (Exception exception){
+
+                }
+                //removing bearer from token
 
             }
         }
